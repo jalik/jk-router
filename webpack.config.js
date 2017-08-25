129e18d5f41081249cd3778548826128424b890a
jalik <karl.stein.pro@gmail.com>
2017-08-25 10:53:12 -1000

    Fixes path separator

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ const filename = Package.name + (isProd ? ".min" : "");
 
 module.exports = {
     entry: {
-        bundle: path.join(__dirname, `src/jk-router.js`)
+        bundle: path.join(__dirname, "src", `${filename}.js`)
     },
     output: {
         libraryTarget: "umd",
